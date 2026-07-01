3ef076cab376c54965a84a30ff9832bc0f9a8f4b
an2834649-gif <an2834649@gmail.com>
2026-07-01 09:40:32 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => res.send('AI Master is LIVE'));
+app.listen(port, () => console.log(`Running on ${port}`));
